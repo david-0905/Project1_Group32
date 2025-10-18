11e0e2f10ad9b757136c4958bfa7b73c764db452
david-0905 <lumipcgs@gmail.com>
2025-10-18 21:23:03 +0800

    Update data integrity check

--- property/fifo_integrity.sv
+++ property/fifo_integrity.sv
@@ -7,9 +7,9 @@ module  fifo_integrity(
     input [3:0] data_out
 );
 
-logic [3:0] sb_mem [3:0]; // Scoreboard memory
-logic [1:0] wr_ptr, rd_ptr;
-logic [2:0] counter;
+logic [3:0] sb_mem [7:0]; // Scoreboard memory
+logic [2:0] wr_ptr, rd_ptr;
+logic [3:0] counter;
 
 logic [3:0] data_out_1t;
 logic [3:0] sb_data_1t;
@@ -18,21 +18,21 @@ logic [3:0] data_out_golden;
 // FIFO Counters and Valid Signals
 always_ff @(posedge clk or negedge rst_b) begin
     if (!rst_b) begin
-        wr_ptr <= 2'd0;
-        rd_ptr <= 2'd0;
-        counter <= 3'd0;
+        wr_ptr <= 3'd0;
+        rd_ptr <= 3'd0;
+        counter <= 4'd0;
     end else begin
         if (vld_in) begin
-            wr_ptr <= wr_ptr + 2'd1;
+            wr_ptr <= wr_ptr + 3'd1;
         end
         if (vld_out) begin
-            rd_ptr <= rd_ptr + 2'd1;
+            rd_ptr <= rd_ptr + 3'd1;
         end
 
         if (vld_in && !vld_out) begin
-            counter <= counter + 3'd1;
+            counter <= counter + 4'd1;
         end else if (!vld_in && vld_out) begin
-            counter <= counter - 3'd1;
+            counter <= counter - 4'd1;
         end
     end
 end
@@ -40,7 +40,7 @@ end
 // Writing data to the Scoreboard
 always_ff @(posedge clk or negedge rst_b) begin
     if (!rst_b) begin
-        for (int i = 0; i < 4; i++) begin
+        for (int i = 0; i < 7; i++) begin
             sb_mem[i] <= 4'd0;
         end
     end else begin
@@ -50,26 +50,26 @@ always_ff @(posedge clk or negedge rst_b) begin
     end
 end
 
+logic [3:0] pre_rd_ptr;
 // Capturing output data and valid signal
 always_ff @(posedge clk or negedge rst_b) begin
   if (!rst_b) begin
     data_out_1t <= 4'd0;
-    data_out_golden <= 4'd0;
+    pre_rd_ptr <= 3'd0;
   end else begin
     data_out_1t <= data_out;
-    if(counter == 3'd0) begin
-      data_out_golden <= data_in;
-    end else begin
-      data_out_golden <= sb_mem[rd_ptr];
-    end
+    pre_rd_ptr <= rd_ptr;
   end
 end
+
+// check
+assign data_out_golden = sb_mem[pre_rd_ptr];
 
 // Overflow Check (Assertion: Overflow when FIFO is full and write occurs without read)
 no_overflow : assert property (
     @(posedge clk)
     disable iff (!rst_b)
-    !(counter == 3'd4 && vld_in && !vld_out)); // FIFO overflow check
+    !(counter == 4'd4 && vld_in && !vld_out)); // FIFO overflow check
 
 // Data Integrity Check (Assertion: Written data should match read data)
 data_integrity : assert property (
